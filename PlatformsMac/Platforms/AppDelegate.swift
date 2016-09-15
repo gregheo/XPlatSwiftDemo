067bcff950e1d0d6e6d286e9f7203be280464a27
Greg Heo <greg@gregheo.com>
2016-09-15 06:17:15 -0500

    First commit! Starter macOS app

---
+++ PlatformsMac/Platforms/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Platforms
+//
+//  Created by Greg Heo on 2016-09-15.
+//  Copyright © 2016 Greg Heo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+}
---
+++ PlatformsMac/Platforms/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Platforms
+//
+//  Created by Greg Heo on 2016-09-15.
+//  Copyright © 2016 Greg Heo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  @IBOutlet weak var tableView: NSTableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+}
