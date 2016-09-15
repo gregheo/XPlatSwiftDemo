0df7d3ec52a6d31cbcc3013e01aedafb6cfbb117
Greg Heo <greg@gregheo.com>
2016-09-15 12:20:25 -0500

    Cross-platform playground code

--- SessionsJSON.playground/Contents.swift
+++ SessionsJSON.playground/Contents.swift
@@ -1,14 +1,31 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
+import Foundation
 
-guard let jsonData = try? Data(contentsOf: URL(fileURLWithPath: "/Users/gth/src/IDS/sessions.json")) else {
+#if os(Linux)
+  typealias RegexType = RegularExpression
+  let fileUrl = URL(fileURLWithPath: "sessions.json")
+#else
+  typealias RegexType = NSRegularExpression
+  let fileUrl = Bundle.main.url(forResource: "sessions", withExtension: "json")!
+#endif
+
+guard let jsonData = try? Data(contentsOf: fileUrl) else {
   fatalError()
 }
 
-do {
-  let decoded = try JSONSerialization.jsonObject(with: jsonData, options: [])
-  print("\(decoded)")
-} catch {
-  print("\(error)")
+guard let sessions = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [[String:String]] else {
+  fatalError()
+}
+
+guard let swiftRegex = try? RegexType(pattern: "swift", options: [.caseInsensitive]) else {
+  fatalError()
+}
+
+for session in sessions {
+  if let speaker = session["speaker"], let title = session["title"] {
+    if swiftRegex.numberOfMatches(in: title, options: [], range: NSMakeRange(0, title.utf8.count)) > 0 {
+      print("\(speaker): \(title)")
+    }
+  }
 }
