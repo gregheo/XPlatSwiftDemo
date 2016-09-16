9b3eea0c96169a12c1f21086e6225e45a50572f7
Greg Heo <greg@gregheo.com>
2016-09-16 10:46:55 -0500

    Working Mac app v1

--- PlatformsMac/Platforms/ViewController.swift
+++ PlatformsMac/Platforms/ViewController.swift
@@ -12,37 +12,58 @@ class ViewController: NSViewController {
 
   @IBOutlet weak var tableView: NSTableView!
 
+  var sessions: [[String: String]] = []
+
   override func viewDidLoad() {
     super.viewDidLoad()
     
     tableView.delegate = self
     tableView.dataSource = self
+    
+    let fileUrl = Bundle.main.url(forResource: "sessions", withExtension: "json")!
+
+    guard let jsonData = try? Data(contentsOf: fileUrl) else {
+      fatalError()
+    }
+
+    do {
+      if let sessions = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [[String:String]] {
+        self.sessions = sessions
+        tableView.reloadData()
+      }
+    } catch {
+      fatalError()
+    }
   }
 
 }
 
 extension ViewController: NSTableViewDataSource {
   func numberOfRows(in tableView: NSTableView) -> Int {
-    return 10
+    return sessions.count
   }
 }
 
 extension ViewController: NSTableViewDelegate {
   func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-    guard let tableColumn = tableColumn else {
+    let session = sessions[row]
+    
+    guard
+      let tableColumn = tableColumn,
+      let cellView = tableView.make(withIdentifier: tableColumn.identifier , owner: nil) as? NSTableCellView,
+      let sessionTitle = session["title"],
+      let sessionSpeaker = session["speaker"],
+      let sessionTime = session["time"]
+    else {
       return nil
     }
-    
-    guard let cellView = tableView.make(withIdentifier: tableColumn.identifier , owner: nil) as? NSTableCellView else {
-      return nil
-    }
-    
+
     if tableColumn == tableView.tableColumns[0] {
-      cellView.textField?.stringValue = "Column zero!"
+      cellView.textField?.stringValue = sessionTime
     } else if tableColumn == tableView.tableColumns[1] {
-      cellView.textField?.stringValue = "Column one!"
+      cellView.textField?.stringValue = sessionTitle
     } else if tableColumn == tableView.tableColumns[2] {
-      cellView.textField?.stringValue = "Column two!"
+      cellView.textField?.stringValue = sessionSpeaker
     }
     
     return cellView
